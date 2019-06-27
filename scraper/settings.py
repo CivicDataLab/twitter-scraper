83f3a8d6442f6d1d2a6370a2850ca059f886b88d
curioswati <swati@civicdatalab.in>
2019-06-27 10:19:13 +0000

    setup base skeleton for project
    - configure settings for the project
    - configure api handler for connecting with the twitter API
    - add function for collecting tweets for a hashtag

---
+++ scraper/settings.py
@@ -0,0 +1,11 @@
+'''
+Project settings.
+'''
+
+import os
+
+
+CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+ACCESS_KEY = os.environ.get('TWITTER_ACCESS_TOKEN')
+ACCESS_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
