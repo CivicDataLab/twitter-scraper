fa791ebe0e3cb8e073afcb6a511e8555dd65e94c
curioswati <swati@civicdatalab.in>
2019-07-03 18:27:45 +0000

    removes extra code from get_tweets
    - it doesn't deal with command line arguments now

--- scraper/get_tweets.py
+++ scraper/get_tweets.py
@@ -1,9 +1,7 @@
 '''
 collects tweets using the twitter API.
 '''
-import sys
 import tweepy
-
 from api import get_api_handler
 
 
@@ -12,14 +10,7 @@ def get_tweets_for_hastag(tag):
     get tweets for a particular hashtag
     '''
     api = get_api_handler()
-    budget_tweets = tweepy.Cursor(api.search, q='#{}'.format(tag),
-                                  rpp=100, languages=['en']).items()
-    for tweet in budget_tweets:
-        print(tweet.text.encode('utf-8'))
+    tweets = tweepy.Cursor(api.search, q='#{}'.format(tag),
+                           rpp=100, languages=['en']).items()
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Please provide the hashtag to search for')
-        sys.exit()
-    HASTAG = sys.argv[1]
-    get_tweets_for_hastag(HASTAG)
+    return tweets
