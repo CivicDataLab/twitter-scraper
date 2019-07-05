4fd8134f09c535172b7f66eacb6fa6e7429bb9ea
DC <deepthi@civicdatalab.in>
2019-07-05 11:36:22 +0530

    Add twitter streaming script

---
+++ scraper/listener.py
@@ -0,0 +1,163 @@
+import sys
+import time
+import csv as csv_lib
+import datetime
+import tweepy
+import simplejson as json
+
+import message_recognizers
+import utils
+
+MISSING_FIELD_VALUE = ''
+
+
+def dump_with_timestamp(text, category="Unknown"):
+    print("(%s)--%s--%s" % (category, datetime.datetime.now(), text))
+
+
+def dump_stream_data(stream_data):
+    dump_with_timestamp(stream_data)
+
+
+class StreamListener(tweepy.StreamListener):
+    """
+    Tweepy StreamListener that dumps tweets to stdout.
+    """
+
+    def __init__(self, opts, logger, api=None):
+        super(StreamListener, self).__init__(api=api)
+        self.opts = opts
+        self.csv_writer = csv_lib.writer(sys.stdout)
+        self.running = True
+        self.first_message_received = None
+        self.status_count = 0
+        self.logger = logger
+
+        # Create a list of recognizer instances, in decreasing priority order.
+        self.recognizers = (
+            message_recognizers.DataContainsRecognizer(
+                handler_method=self.parse_status_and_dispatch,
+                match_string='"in_reply_to_user_id_str":'),
+
+            message_recognizers.DataContainsRecognizer(
+                handler_method=self.parse_limit_and_dispatch,
+                match_string='"limit":{'),
+
+            message_recognizers.DataContainsRecognizer(
+                handler_method=self.parse_warning_and_dispatch,
+                match_string='"warning":'),
+
+            message_recognizers.DataContainsRecognizer(
+                handler_method=self.on_disconnect,
+                match_string='"disconnect":'),
+
+            # Everything else is sent to logger
+            message_recognizers.MatchAnyRecognizer(
+                handler_method=self.on_unrecognized),
+        )
+
+    def on_unrecognized(self, stream_data):
+        self.logger.warn("Unrecognized: %s", stream_data.strip())
+
+    def on_disconnect(self, stream_data):
+        msg = json.loads(stream_data)
+        self.logger.warn("Disconnect: code: %d stream_name: %s reason: %s",
+                         utils.resolve_with_default(msg, 'disconnect.code', 0),
+                         utils.resolve_with_default(msg, 'disconnect.stream_name', 'n/a'),
+                         utils.resolve_with_default(msg, 'disconnect.reason', 'n/a'))
+
+    def parse_warning_and_dispatch(self, stream_data):
+        try:
+            warning = json.loads(stream_data).get('warning')
+            return self.on_warning(warning)
+        except json.JSONDecodeError:
+            self.logger.exception("Exception parsing: %s" % stream_data)
+            return False
+
+    def parse_status_and_dispatch(self, stream_data):
+        """
+        Process an incoming status message.
+        """
+        status = tweepy.models.Status.parse(self.api, json.loads(stream_data))
+        if self.tweet_matchp(status):
+            self.status_count += 1
+            if self.should_stop():
+                self.running = False
+                return False
+        print(stream_data.strip())
+
+    def parse_limit_and_dispatch(self, stream_data):
+        return self.on_limit(json.loads(stream_data)['limit']['track'])
+
+    def is_retweet(self, tweet):
+        return hasattr(tweet, 'retweeted_status') \
+               or tweet.text.startswith('RT ') \
+               or ' RT ' in tweet.text
+
+    def tweet_matchp(self, tweet):
+        """Return True if tweet matches selection criteria...
+        Currently this filters on self.opts.lang if it is not nothing...
+        """
+        if self.opts.no_retweets and self.is_retweet(tweet):
+            return False
+
+        else:
+            return True
+
+    def on_warning(self, warning):
+        self.logger.warn("Warning: code=%s message=%s" % (warning['code'], warning['message']))
+        # If code='FALLING_BEHIND' buffer state is in warning['percent_full']
+
+    def on_error(self, status_code):
+        self.logger.error("StreamListener.on_error: %r" % status_code)
+        if status_code != 401:
+            self.logger.error(" -- stopping.")
+            # Stop on anything other than a 401 error (Unauthorized)
+            # Stop main loop.
+            self.running = False
+            return False
+
+    def on_timeout(self):
+        """Called when there's a timeout in communications.
+        Return False to stop processing.
+        """
+        self.logger.warn('on_timeout')
+        return  ## Continue streaming.
+
+    def on_data(self, data):
+        if not self.first_message_received:
+            self.first_message_received = int(time.time())
+
+        if self.should_stop():
+            self.running = False
+            return False  # Exit main loop.
+
+        for r in self.recognizers:
+            if r.match(data):
+                if r.handle_message(data) is False:
+                    # Terminate main loop.
+                    self.running = False
+                    return False  # Stop streaming
+                # Don't execute any other recognizers, and don't call base
+                # on_data() because we've already handled the message.
+                return
+        # Don't execute any of the base class on_data() handlers.
+        return
+
+    def should_stop(self):
+        """
+        Return True if processing should stop.
+        """
+        if self.opts.duration:
+            if self.first_message_received:
+                et = int(time.time()) - self.first_message_received
+                flag = et >= self.opts.duration
+                if flag:
+                    self.logger.debug("Stop requested due to duration limits (et=%d, target=%d seconds).",
+                                      et,
+                                      self.opts.duration)
+                return flag
+        if self.opts.max_tweets and self.status_count > self.opts.max_tweets:
+            self.logger.debug("Stop requested due to count limits (%d)." % self.opts.max_tweets)
+            return True
+        return False
---
+++ scraper/streamer.py
@@ -0,0 +1,103 @@
+import sys
+import time
+
+import logging
+import os
+import tweepy
+
+import args
+import utils
+from listener import StreamListener
+
+logger = logging.getLogger(__name__)
+
+RETRY_LIMIT = 10
+
+"""
+    List of named location query (--location-query) names and their
+    associated bounding boxes (longitude, latitude) (what is the name of that standard?  geoJSON?)
+    Recursive lookups:
+    If a value is string, it's a reference to a named entry in the table.  Use the remainder of the
+    value string as the new lookup key.
+"""
+
+
+def make_filter_args(opts):
+    kwargs = {}
+    if opts.track:
+        kwargs['track'] = opts.track
+    if opts.stall_warnings:
+        kwargs['stall_warnings'] = True
+    if opts.follow:
+        kwargs['follow'] = opts.follow
+    return kwargs
+
+
+def process_tweets(opts):
+    """Set up and process incoming streams."""
+    try:
+        consumer_key = os.environ['CONSUMER_KEY']
+        consumer_secret = os.environ['CONSUMER_SECRET']
+        access_key = os.environ['ACCESS_KEY']
+        access_secret = os.environ['ACCESS_SECRET']
+        logger.debug('consumer_key={consumer_key}, consumer_secret={consumer_secret}'.format(**locals()))
+        logger.debug('access_key={access_key}, access_secret={access_secret}'.format(**locals()))
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_key, access_secret)
+    except KeyError as e:
+        logger.error('You must set the %s API key environment variable.', e)
+        return
+
+    logger.debug('Init tweepy.Stream()')
+    logger.debug(opts)
+    listener = StreamListener(opts=opts, logger=logger)
+    streamer = tweepy.Stream(auth=auth, listener=listener, retry_count=9999,
+                             retry_time=1, buffer_size=16000)
+
+    try:
+        kwargs = make_filter_args(opts)
+    except ValueError as e:
+        listener.running = False
+        sys.stderr.write("%s: error: %s\n" % (__file__, e.message))
+        return
+
+    while listener.running:
+        try:
+            try:
+                logger.debug('streamer.filter(%s)' % kwargs)
+                streamer.filter(**kwargs)
+            except TypeError as e:
+                if 'stall_warnings' in e.message:
+                    logger.warn(
+                        "Installed Tweepy version doesn't support stall_warnings parameter.  Restarting without stall_warnings parameter.")
+                    del kwargs['stall_warnings']
+                    streamer.filter(**kwargs)
+                else:
+                    raise
+
+            logger.debug('Returned from streaming...')
+        except IOError:
+            if opts.terminate_on_error:
+                listener.running = False
+            logger.exception('Caught IOError')
+        except KeyboardInterrupt:
+            # Stop the listener loop.
+            listener.running = False
+        except Exception:
+            listener.running = False
+            logger.exception("Unexpected exception.")
+
+        if listener.running:
+            logger.debug('Sleeping...')
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    opts = args.parse_command_line()
+
+    if not opts.track:
+        sys.stderr.write(
+            '%s: error: Must provide list of track keywords.\n' % __file__)
+        sys.exit()
+    utils.init_logger(logger, opts.log_level)
+    process_tweets(opts)
